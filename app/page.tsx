1517457d01078e1fbdd0c7d62bdd11c4a68e052b
apacinate <apacinate@outlook.jp>
2025-04-16 11:40:06 +0900

    allItems型指定

--- app/page.tsx
+++ app/page.tsx
@@ -3,18 +3,29 @@ import Image from "next/image"
 
 export const dynamic = 'force-dynamic'
 
-const getAllItems = async() =>{
-  const response = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/item/readall`)
-  const jsonData = await response.json()
-  const allItems = jsonData.allItems
-  return allItems
+// アイテムの型定義
+interface Item {
+  id: string;
+  image: Object;
+  price: number;
+  title: string;
+  description: string;
+}
+
+
+// getAllItems 関数に戻り値の型を付与
+const getAllItems = async (): Promise<Item[]> => {
+  const response = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/item/readall`);
+  const jsonData = await response.json();
+  const allItems = jsonData.allItems;
+  return allItems;
 } 
 
 const ReadAllItems = async() => {
-  const allItems = await getAllItems()
+  const allItems: Item[] = await getAllItems()
   return(
     <div className="grid-container-in">
-      {allItems.map(item=>
+      {allItems.map((item)=>
         <Link href={`/item/readsingle/${item.id}`} key={item.id}>
           <Image src={item.image} width={750} height={500} alt="item-image" priority/>
           <h2>{item.price}</h2>
